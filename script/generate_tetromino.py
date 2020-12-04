63e5a027c7463451e592d560c4308b04692b282d
Ikemen Mas Kot <maskot1977@gmail.com>
2020-12-04 10:33:50 +0900

    Update generate_tetromino.py

--- script/generate_tetromino.py
+++ script/generate_tetromino.py
@@ -5,10 +5,10 @@ from boxing_puzzle import boxing2
 
 os.system("mkdir tetromino/")
 for i, matrix in enumerate(find_some(
-        x_length = 10,
-        y_length = 6,
-        piece_size = 5,
-        same_piece_limit = 1,
-        max_trial = 5300000
+        x_length = 8,
+        y_length = 5,
+        piece_size = 4,
+        same_piece_limit = 2,
+        max_trial = 5
     )):
     pd.DataFrame(matrix).to_csv("tetromino/{}.csv".format(i))
