1ea1b2ee99f62c8abd8f43d9b4f4d0888a7c2067
Masaaki Kotera <kotera@preferred.jp>
2020-12-04 04:18:02 +0000

    fixed generate_pentomino.py

--- script/generate_pentomino.py
+++ script/generate_pentomino.py
@@ -4,11 +4,11 @@ import pandas as pd
 from boxing_puzzle import boxing2
 
 os.system("mkdir pentomino/")
-for i, matrix in enumerate(find_some(
+for i, matrix in enumerate(boxing2.find_some(
         x_length = 10,
         y_length = 6,
         piece_size = 5,
         same_piece_limit = 1,
-        max_trial = 530000
+        max_trial = 5
     )):
     pd.DataFrame(matrix).to_csv("pentomino/{}.csv".format(i))
